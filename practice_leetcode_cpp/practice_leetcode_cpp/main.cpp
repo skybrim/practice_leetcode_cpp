9a36aaeaee8476226c397e036567d0d47a580a6e
wiley <throughskybrim@gmail.com>
2020-12-12 13:26:27 +0800

    xcode

---
+++ practice_leetcode_cpp/practice_leetcode_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice_leetcode_cpp
+//
+//  Created by wiley on 2020/12/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
