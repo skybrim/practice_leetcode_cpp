38ca1d35d9eb7c289c55f85b696c37b206927cec
wiley <throughskybrim@gmail.com>
2020-12-16 10:26:47 +0800

    offer offer_10-I

---
+++ practice_leetcode_cpp/practice_leetcode_cpp/offer/offer_10-I.cpp
@@ -0,0 +1,45 @@
+//
+//  offer_10-I.cpp
+//  practice_leetcode_cpp
+//
+//  Created by wiley on 2020/12/16.
+//
+/*
+ 剑指 Offer 10- I. 斐波那契数列
+ 
+ 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+ F(0) = 0,   F(1) = 1
+ F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+ 示例 1：
+ 输入：n = 2
+ 输出：1
+ 
+ 示例 2：
+ 输入：n = 5
+ 输出：5
+  
+ 提示：
+ 0 <= n <= 100
+*/
+#include <stdio.h>
+class Solution {
+public:
+    int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int res = 0;
+        for (int i = 2; i < n + 1; ++i) {
+            res = a + b;
+            res %= 1000000007;
+            a = b % 1000000007;
+            b = res;
+        }
+        return res;
+    }
+};
