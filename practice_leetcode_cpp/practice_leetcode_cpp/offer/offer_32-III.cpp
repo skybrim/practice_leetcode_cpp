b2f083a5481d56e93d6cdd5b9dead394ff5b4792
wiley <throughskybrim@gmail.com>
2021-02-05 10:24:55 +0800

    offer offer_32-III

--- practice_leetcode_cpp/practice_leetcode_cpp/offer/offer_32-III.cpp
+++ practice_leetcode_cpp/practice_leetcode_cpp/offer/offer_32-III.cpp
@@ -1,13 +1,13 @@
 //
-//  offer_32-II.cpp
+//  offer_32-III.cpp
 //  practice_leetcode_cpp
 //
-//  Created by wiley on 2021/2/1.
+//  Created by wiley on 2021/2/5.
 //
 /**
- 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ 剑指 Offer 32 - III. 从上到下打印二叉树 III
 
- 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
 
  例如:
  给定二叉树: [3,9,20,null,null,15,7],
@@ -19,7 +19,7 @@
  返回其层次遍历结果：
  [
    [3],
-   [9,20],
+   [20,9],
    [15,7]
  ]
 
@@ -28,8 +28,10 @@
  */
 #include <stdio.h>
 #include <vector>
-#include <deque>
+#include <queue>
+
 using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -44,21 +46,24 @@ public:
             return {};
         }
         vector<vector<int>> ret;
-        deque<TreeNode *> help_deq;
-        help_deq.push_back(root);
-        while (!help_deq.empty()) {
+        queue<TreeNode *> help_queue;
+        help_queue.push(root);
+        while (!help_queue.empty()) {
             vector<int> temp;
-            size_t count = help_deq.size();
+            size_t count = help_queue.size();
             while (count--) {
-                TreeNode *cur = help_deq.front();
-                help_deq.pop_front();
+                TreeNode *cur = help_queue.front();
+                help_queue.pop();
                 temp.push_back(cur->val);
                 if (cur->left) {
-                    help_deq.push_back(cur->left);
+                    help_queue.push(cur->left);
                 }
                 if (cur->right) {
-                    help_deq.push_back(cur->right);
+                    help_queue.push(cur->right);
                 }
+            }
+            if ((ret.size() & 1) != 0) {
+                reverse(temp.begin(), temp.end());
             }
             ret.push_back(temp);
         }
